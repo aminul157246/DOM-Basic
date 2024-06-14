cd8e90309fdd9e913f92fcf2a97f85694e640102
aminul157246 <aminul.islam.157246@gmail.com>
2024-06-15 01:54:12 +0600

    event update

--- js/updateInput.js
+++ js/updateInput.js
@@ -1,6 +1,6 @@
 const getDefaultText = document.getElementById('update-btn').addEventListener('click', function(){
     const inputField = document.getElementById('input-text')
-    const inputText = inputField.value
+    const inputText = inputField.value;
 
     const defaultText = document.getElementById('default-text')
     // console.log(defaultText);
